beaa5eb50d819493567ca11318827e4f8f6d37a7
andremanroes <andre.manurung@tiket.com>
2023-10-14 16:13:51 +0700

    tree commit fix

--- src/main/com/example/HelloWorldApp.java
+++ src/main/com/example/HelloWorldApp.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World Version 1");
     }
 }
